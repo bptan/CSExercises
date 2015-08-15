b25d5fc0dd1e3d617f021282228cbf5ff8e5a28a
Boon-Piao Tan <t.boonpiao@gmail.com>
2015-08-15 12:53:37 +0800

    did ex 11

--- CSExercises/Ex07-15/Ex11.cs
+++ CSExercises/Ex07-15/Ex11.cs
@@ -10,7 +10,10 @@ namespace CSExercises
     {
         public static void Main(string[] args)
         {
-            
+            Console.Write("Please enter the distance travelled in km: ");
+            double distance = Convert.ToDouble(Console.ReadLine());
+            double fare = 2.40 + distance * 0.4;
+            Console.WriteLine("The total fare is {0}", fare);
         }
     }
 }

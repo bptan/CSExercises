f7044cef46f6c4fc939224f0ac68b29623e99859
Boon-Piao Tan <t.boonpiao@gmail.com>
2015-08-18 15:21:28 +0800

    completed ex34

--- LocalScore.cmd
+++ LocalScore.cmd
@@ -1,6 +1,6 @@
 @echo Testing your project
 @pushd .
 @copy CSExercises\bin\debug\*.exe .
-@if [%1] NEQ [] "%VS120COMNTOOLS%\..\ide\commonextensions\microsoft\testwindow\vstest.console.exe" ExercisesTest.dll /Logger:trx /Tests:%1
-@if [%1] == [] "%VS120COMNTOOLS%\..\ide\commonextensions\microsoft\testwindow\vstest.console.exe" ExercisesTest.dll /Logger:trx
+@if [%1] NEQ [] "%VS140COMNTOOLS%\..\ide\commonextensions\microsoft\testwindow\vstest.console.exe" ExercisesTest.dll /Logger:trx /Tests:%1
+@if [%1] == [] "%VS140COMNTOOLS%\..\ide\commonextensions\microsoft\testwindow\vstest.console.exe" ExercisesTest.dll /Logger:trx
 @popd

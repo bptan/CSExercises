f035e3aecf5508981c95282e397e09a47ede71f6
bptan <t.boonpiao@gmail.com>
2015-08-20 16:27:05 +0800

    ex 46

---
+++ CSExercises/SnakeAndLadder.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSExercises
+{
+    class SnakeAndLadder
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
